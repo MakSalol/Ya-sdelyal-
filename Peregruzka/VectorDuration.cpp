e2f7f50fb4236476f8400188c49f28e0f07e094b
MakSa <maximuszheldor@mail.ru>
2018-11-16 19:32:16 +0300

    VectorDuration

--- Peregruzka/VectorDuration.cpp
+++ Peregruzka/VectorDuration.cpp
@@ -8,17 +8,15 @@ class  Duration {
 public:
 	int hour;
 	int min;
-	void change(int h, int m){
-		h=hour;
-		m=min;
-		hour = hour + min/60;
-		min = min % 60;
-	}
+		 Duration (int h = 0, int m = 0){
+		int total = h*60 + m;
+		hour = total / 60;
+		min = total % 60;
+		 }
 	};
 
 ostream& operator<<(ostream& stream, Duration& dur)
 {
-	dur.change(dur.hour, dur.min);
 stream<< setfill('0');
 stream<< setw(2) << dur.hour << ":" << setw(2) << dur.min;
 
@@ -26,45 +24,46 @@ return stream;
 }
 
 istream& operator>>(istream& stream, Duration& dur){
-	stream >> dur.hour >> dur.min;
-	return stream;
+ stream >> dur.hour >> dur.min;
+ return stream;
 }
-
+bool operator<(Duration& dur1, Duration dur2){
+	if (dur1.hour == dur2.hour){
+		return dur1.min < dur2.min;
+	}
+	return dur1.hour < dur2.hour;
+}
 
 bool operator>(Duration& dur1, Duration& dur2){
+	if (dur1.hour == dur2.hour){
+		return dur1.min > dur2.min;
+	}
 	return dur1.hour > dur2.hour;
-	return dur1.min > dur2.min;
 }
 
-bool operator<(Duration& dur1, Duration& dur2){
-	return dur1.hour < dur2.hour;
-	return dur1.min < dur2.min;
+Duration operator-(Duration& dur1, Duration& dur2){
+	if (dur1 < dur2){
+		Duration dur3(0,0);
+		return dur3;
+	}
+	Duration dur3(dur1.hour - dur2.hour, dur1.min - dur2.min);
+	return dur3;
 }
-
-
 
 int main() {
-	vector<Duration> a;
-	Duration dur1;
-		cin >> dur1;
-		dur1.change(dur1.hour, dur1.min);
-		a.push_back(dur1);
-	Duration dur2;
-		cin >> dur2;
-		dur2.change(dur2.hour, dur2.min);
-		a.push_back(dur2);
-	Duration dur3;
-		cin >> dur3;
-		dur3.change(dur3.hour, dur3.min);
-		a.push_back(dur3);
-			auto SortDuration = [&a](){
-	sort(a.begin(), a.end());
-		for (int i = 0; i < a.size(); i++){
-			cout << a[i] << " ";
-		}
-	};
-		SortDuration();
+	vector <Duration> a;
+	Duration dur1(1,20);
+	Duration dur2(2,30);
+	Duration dur3(1,70);
+	a.push_back(dur1);
+	a.push_back(dur2);
+	a.push_back(dur3);
+	auto SortDuration = [&a](){
+		sort(a.begin(), a.end());
+			for (int i = 0; i < a.size(); i++){
+				cout << a[i] << " ";
+			}
+		};
+			SortDuration();
 
-}
-
-
+	}
